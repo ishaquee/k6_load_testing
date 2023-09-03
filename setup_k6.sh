434d0c831bfd648bc2b3c08bc0db8be9501785ce
mohamed <ishaque0310@gmail.com>
2023-09-04 00:07:32 +0530

    Added Jenkins file

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,8 +1,5 @@
-set -ex
-
-sudo apt-get update
-sudo apt-get install dirmngr --install-recommends
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
-sudo apt-get update
-sudo apt-get install k6
+jenkins ALL=(ALL) NOPASSWD: /usr/bin/apt-get update
+jenkins ALL=(ALL) NOPASSWD: /usr/bin/apt-get install dirmngr --install-recommends
+jenkins ALL=(ALL) NOPASSWD: /usr/bin/apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+jenkins ALL=(ALL) NOPASSWD: /usr/bin/tee /etc/apt/sources.list.d/k6.list
+jenkins ALL=(ALL) NOPASSWD: /usr/bin/apt-get install k6
